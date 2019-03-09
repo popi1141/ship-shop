fc4b4fcf4653f10a73345339baa2a75ea5b7dbe0
Nathanael <nathanael.davidson@gmail.com>
2019-03-09 10:43:22 -0800

    lots

--- frontend/src/app/product-data-chart/product-data-chart.component.ts
+++ frontend/src/app/product-data-chart/product-data-chart.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Chart from 'chart.js';
+
+class DataPoint {
+  x: number;
+  y: number;
+}
+
+class DataSet {
+  data: Array<DataPoint>;
+  baseline: number;
+}
 
 @Component({
   selector: 'app-product-data-chart',
@@ -6,10 +17,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-data-chart.component.css']
 })
 export class ProductDataChartComponent implements OnInit {
-
+  @Input() dataSets: {[key: string]: DataSet};
+  title = '';
+  private chart: Chart;
   constructor() { }
 
   ngOnInit() {
+    this.chart = new Chart('dataChart', {
+      type: 'line',
+      data: {
+        datasets: [{
+          fill: 1,
+          label: 'Measured Value',
+          data: [],
+        }, {
+          label: 'Stated Value',
+          data: [],
+        }]
+      },
+      options: {
+        legend: {
+          position: 'bottom'
+        },
+        title: {
+          text: this.title
+        },
+        layout: {
+          padding: {
+            top: 10
+          }
+        },
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+    });
+    this.showData('Temperature');
   }
 
+  showData(label) {
+    const dataSet = this.dataSets[label];
+    this.chart.data.datasets[0].data = dataSet.data;
+    this.chart.data.datasets[1].data = [{x: 0, y: dataSet.baseline}, {x: dataSet.data.length - 1, y: dataSet.baseline}];
+    this.title = label;
+    this.chart.update();
+  }
 }
--- frontend/src/app/product-detail/product-detail.component.ts
+++ frontend/src/app/product-detail/product-detail.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../product';
 
+
 @Component({
-  selector: 'app-product',
+  selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor(public product: Product) { }
+  @Input() product: Product;
+  constructor() { }
 
   ngOnInit() {
   }
